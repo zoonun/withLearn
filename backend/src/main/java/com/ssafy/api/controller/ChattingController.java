2a69161b871e581d88b2d01325c2a1c4a36b24a6
배태호 <tdj03063@gmail.com>
2021-07-29 16:41:06 +0900

    Feat : Add chatting save code 1

--- backend/src/main/java/com/ssafy/api/controller/ChattingController.java
+++ backend/src/main/java/com/ssafy/api/controller/ChattingController.java
@@ -1,5 +1,7 @@
 package com.ssafy.api.controller;
 
+import com.ssafy.api.service.ChattingService;
+import com.ssafy.api.service.ConferenceService;
 import com.ssafy.db.entity.Chatting;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
@@ -7,30 +9,34 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 인증 관련 API 요청 처리를 위한 컨트롤러 정의.
  */
 @Profile("stomp")
+@RestController
 public class ChattingController {
-   private final SimpMessagingTemplate template;
 
-   @Autowired
-   public ChattingController(SimpMessagingTemplate template) {
-      this.template = template;
-   }
+    @Autowired
+    ChattingService chattingService;
 
-//   @MessageMapping("/chat/join")
-//   public void join(Chatting message) {
-//      message.setContents(message.getUser().getName() + "님이 입장하셨습니다.");
-//      template.convertAndSend("/topic/chat" + message.getConference().getId(), message);
-//   }
+    private final SimpMessagingTemplate template;
 
-   @MessageMapping("/chat/{conferenceId}")
-   public void sendMessage(@DestinationVariable Long conferenceId, Chatting message){
-//      Chatting chat = new Chatting();
-//      chat.setContents(chatting.get);
-      System.out.println("send message: "+ message + " to : "+ conferenceId);
-      template.convertAndSend("/topic/message",message);
-   }
+    @Autowired
+    public ChattingController(SimpMessagingTemplate template) {
+        this.template = template;
+    }
+
+    @MessageMapping("/chat/join/{conferenceId}")
+    public void join(@DestinationVariable Long conferenceId, Chatting message) {
+        template.convertAndSend("/topic/chat/" + conferenceId, message.getUser().getName() + "님이 입장하셨습니다.");
+    }
+
+    @MessageMapping("/chat/message/{conferenceId}")
+    public void message(@DestinationVariable Long conferenceId, Chatting message) {
+        System.out.println(message);
+        chattingService.saveChatting(message);
+        template.convertAndSend("/topic/chat/" + conferenceId, message);
+    }
 }
---
+++ backend/src/main/java/com/ssafy/api/domain/Chat.java
@@ -0,0 +1,4 @@
+package com.ssafy.api.domain;
+
+public class Chat {
+}
