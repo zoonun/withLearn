a50cf8ffd3cc8f79c195361900518e3ce9382038
배태호 <tdj03063@gmail.com>
2021-08-03 09:50:13 +0900

    Refactor : Change imagepath

--- backend/src/main/java/com/ssafy/api/service/ConferenceServiceImpl.java
+++ backend/src/main/java/com/ssafy/api/service/ConferenceServiceImpl.java
@@ -37,52 +37,18 @@ public class ConferenceServiceImpl implements ConferenceService  {
 
     @Override
     public Conference createConference(String description, String title, Long conferenceCategoryId, MultipartFile thumbnail,  String conferenceDay, Date conferenceTime, Date applyEndTime, Date applyStartTime, Integer price ) throws IOException{
-        System.out.println(2);
         Conference conference = new Conference();
         ConferenceCategory conferenceCategory = conferenceCategoryRepository.findById(conferenceCategoryId).get();
-        conference.setIs_active(true);
-        conference.setIs_free(true);
-        conference.setDescription(description);
+        conference.setIs_active(false);
         conference.setTitle(title);
+        conference.setDescription(description);
+        conference.setConferenceCategory(conferenceCategory);
         conference.setConference_day(conferenceDay);
         conference.setConference_time(conferenceTime);
-        conference.setApply_end_time(applyEndTime);
         conference.setApply_start_time(applyStartTime);
+        conference.setApply_end_time(applyEndTime);
         conference.setPrice(price);
-        conference.setConferenceCategory(conferenceCategory);
-
-        // 파일 이름을 업로드 한 날짜로 바꾸어서 저장할 것이다
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
-        String current_date = simpleDateFormat.format(new Date());
-        System.out.println(3 + " " + current_date + " current_date");
-        // 프로젝트 폴더에 저장하기 위해 절대경로를 설정 (Window 의 Tomcat 은 Temp 파일을 이용한다)
-        String absolutePath = new File("").getAbsolutePath() + "\\";
-
-        System.out.println("4 : " + absolutePath);
-
-        // 경로를 지정하고 그곳에다가 저장할 심산이다
-        String path = "images/" + current_date;
-        File file = new File(path);
-        // 저장할 위치의 디렉토리가 존지하지 않을 경우
-        if (!file.exists()) {
-            // mkdir() 함수와 다른 점은 상위 디렉토리가 존재하지 않을 때 그것까지 생성
-            file.mkdirs();
-        }
-        System.out.println(5 + ": path");
-        if (thumbnail != null) {
-            // jpeg, png, gif 파일들만 받아서 처리할 예정
-            String originalFileExtension = thumbnail.getOriginalFilename().substring(thumbnail.getOriginalFilename().lastIndexOf("."));
-
-            System.out.println("6 : " + originalFileExtension);
-            // 각 이름은 겹치면 안되므로 나노 초까지 동원하여 지정
-            String new_file_name = Long.toString(System.nanoTime()) + originalFileExtension;
-
-            // 저장된 파일로 변경하여 이를 보여주기 위함
-            file = new File(absolutePath + path + "/" + new_file_name);
-            thumbnail.transferTo(file);
-            conference.setThumbnail(new_file_name);
-            System.out.println(new_file_name + " new_file_name");
-        }
+        conference.setThumbnail(setThumbnail(conference,thumbnail));
         return conferenceRepository.save(conference);
     }
 
@@ -90,48 +56,40 @@ public class ConferenceServiceImpl implements ConferenceService  {
     public void patchConferenceInfo(String description, String title, Long conferenceCategoryId, MultipartFile thumbnail, String conferenceDay, Date conferenceTime, Date applyEndTime, Date applyStartTime, Boolean isActive, Integer price, Long conference_id) throws IOException {
         Conference conference = conferenceRepositorySupport.findConferenceByConferenceId(conference_id).get();
         ConferenceCategory conferenceCategory = conferenceCategoryRepository.findById(conferenceCategoryId).get();
-        conference.setConference_time(conferenceTime);
-        conference.setConference_day(conferenceDay);
-        conference.setApply_start_time(applyStartTime);
-        conference.setApply_end_time(applyEndTime);
-        conference.setIs_active(isActive);
-        conference.setPrice(price);
         conference.setTitle(title);
         conference.setDescription(description);
         conference.setConferenceCategory(conferenceCategory);
+        conference.setConference_day(conferenceDay);
+        conference.setConference_time(conferenceTime);
+        conference.setApply_start_time(applyStartTime);
+        conference.setApply_end_time(applyEndTime);
+        conference.setPrice(price);
+        conference.setThumbnail(setThumbnail(conference,thumbnail));
+        conferenceRepository.save(conference);
+    }
 
+    public String setThumbnail(Conference conference,MultipartFile thumbnail) throws IOException {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
         String current_date = simpleDateFormat.format(new Date());
-        System.out.println(3 + " " + current_date + " current_date");
-        // 프로젝트 폴더에 저장하기 위해 절대경로를 설정 (Window 의 Tomcat 은 Temp 파일을 이용한다)
+
         String absolutePath = new File("").getAbsolutePath() + "\\";
+        absolutePath = absolutePath.substring(0,absolutePath.length()-8);
 
-        System.out.println("4 : " + absolutePath);
-        System.out.println(thumbnail.getOriginalFilename());
-        // 경로를 지정하고 그곳에다가 저장할 심산이다
-        String path = "images/" + current_date;
+        String path = absolutePath + "images/" + current_date;
         File file = new File(path);
-        // 저장할 위치의 디렉토리가 존지하지 않을 경우
-        if (!file.exists()) {
-            // mkdir() 함수와 다른 점은 상위 디렉토리가 존재하지 않을 때 그것까지 생성
+        if (!file.exists())
             file.mkdirs();
-        }
-        System.out.println(5 + ": path");
-        if (thumbnail != null) {
-            // jpeg, png, gif 파일들만 받아서 처리할 예정
-            String originalFileExtension = thumbnail.getOriginalFilename().substring(thumbnail.getOriginalFilename().lastIndexOf("."));
 
-            System.out.println("6 : " + originalFileExtension);
-            // 각 이름은 겹치면 안되므로 나노 초까지 동원하여 지정
+        if (thumbnail != null) {
+            String originalFileExtension = thumbnail.getOriginalFilename().substring(thumbnail.getOriginalFilename().lastIndexOf("."));
             String new_file_name = Long.toString(System.nanoTime()) + originalFileExtension;
 
-            // 저장된 파일로 변경하여 이를 보여주기 위함
-            file = new File(absolutePath + path + "/" + new_file_name);
+            file = new File( path + "/" + new_file_name);
             thumbnail.transferTo(file);
-            conference.setThumbnail(new_file_name);
-            System.out.println(new_file_name + " new_file_name");
+            conference.setThumbnail("images/"+current_date+"/"+new_file_name);
+            return "images/"+current_date+"/"+new_file_name;
         }
-        conferenceRepository.save(conference);
+        return "";
     }
 
     @Override
@@ -145,57 +103,6 @@ public class ConferenceServiceImpl implements ConferenceService  {
         Conference conference = conferenceRepositorySupport.findConferenceByConferenceId(conferenceId).get();
         return conference;
     }
-
-    @Override
-    public void patchConferenceInfo(Long conferenceId, String description, String title, Long conferenceCategoryId, MultipartFile thumbnail,  String conferenceDay, Date conferenceTime, Date applyEndTime, Date applyStartTime, Integer price, Boolean isFree, Boolean isActive)  throws IOException{
-//        Conference conference = conferenceRepositorySupport.findConferenceByConferenceId(conferenceId).get();
-//        ConferenceCategory conferenceCategory = new ConferenceCategory();
-//        conference.setConference_time(conferenceTime);
-//        conference.setConference_day(conferenceDay);
-//        conference.setApply_start_time(applyStartTime);
-//        conference.setApply_end_time(applyEndTime);
-//        conference.setIs_active(isActive);
-//        conference.setIs_free(isFree);
-//        conference.setPrice(price);
-//        conference.setTitle(title);
-//        conference.setDescription(description);
-//        conferenceCategory.setId(conferenceCategoryId);
-//        conference.setConferenceCategory(conferenceCategory);
-//
-//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
-//        String current_date = simpleDateFormat.format(new Date());
-//        System.out.println(3 + " " + current_date + " current_date");
-//        // 프로젝트 폴더에 저장하기 위해 절대경로를 설정 (Window 의 Tomcat 은 Temp 파일을 이용한다)
-//        String absolutePath = new File("").getAbsolutePath() + "\\";
-//
-//        System.out.println("4 : " + absolutePath);
-//
-//        // 경로를 지정하고 그곳에다가 저장할 심산이다
-//        String path = "images/" + current_date;
-//        File file = new File(path);
-//        // 저장할 위치의 디렉토리가 존지하지 않을 경우
-//        if (!file.exists()) {
-//            // mkdir() 함수와 다른 점은 상위 디렉토리가 존재하지 않을 때 그것까지 생성
-//            file.mkdirs();
-//        }
-//        System.out.println(5 + ": path");
-//        if (thumbnail != null) {
-//            // jpeg, png, gif 파일들만 받아서 처리할 예정
-//            String originalFileExtension = thumbnail.getOriginalFilename().substring(thumbnail.getOriginalFilename().lastIndexOf("."));
-//
-//            System.out.println("6 : " + originalFileExtension);
-//            // 각 이름은 겹치면 안되므로 나노 초까지 동원하여 지정
-//            String new_file_name = Long.toString(System.nanoTime()) + originalFileExtension;
-//
-//            // 저장된 파일로 변경하여 이를 보여주기 위함
-//            file = new File(absolutePath + path + "/" + new_file_name);
-//            thumbnail.transferTo(file);
-//            conference.setThumbnail(new_file_name);
-//            System.out.println(new_file_name + " new_file_name");
-//        }
-//        conferenceRepository.save(conference);
-    }
-
 
     @Override
     public Optional<List<Conference>> getAllConference(String title, String sort, Integer size, Long conferenceCategory) {
