c028da6f9ae22887f4a5988658abca5085cd45a5
배태호 <tdj03063@gmail.com>
2021-08-04 11:20:51 +0900

    Feat : Add conference delete code && refactoring

--- backend/src/main/java/com/ssafy/api/service/ConferenceService.java
+++ backend/src/main/java/com/ssafy/api/service/ConferenceService.java
@@ -23,6 +23,8 @@ public interface ConferenceService {
 	Optional<ConferenceCategory> getConferenceCategoryByName(String name);
 	void deleteConferenceCategory(long categoryId);
 	Optional<List<UserConference>> getUserConferenceByConferenceId(Long conference_id);
-    Conference createConference(String description, String title, Long conferenceCategory_id, String thumbnail, String conferenceDay, Date conferenceTime, Date applyEndTime, Date applyStartTime, Integer price) throws IOException;
-	void patchConferenceInfo(String description, String title, Long conferenceCategoryId, String thumbnail, String conferenceDay, Date conferenceTime, Date applyEndTime, Date applyStartTime, Boolean isActive, Integer price, Long conference_id) throws IOException ;
+    Conference createConference(String description, String title, Long conferenceCategory_id, String thumbnail, String conferenceDay, Date conferenceTime, Date applyEndTime, Date applyStartTime, Integer price);
+	void patchConferenceInfo(String description, String title, Long conferenceCategoryId, String thumbnail, String conferenceDay, Date conferenceTime, Date applyEndTime, Date applyStartTime, Boolean isActive, Integer price, Long conference_id);
+	Conference setConference(Conference conference, ConferenceCategory conferenceCategory, String description, String title, String thumbnail, String conferenceDay, Date conferenceTime, Date applyEndTime, Date applyStartTime, Integer price);
+	void deleteConference(Long conference_id);
 }
