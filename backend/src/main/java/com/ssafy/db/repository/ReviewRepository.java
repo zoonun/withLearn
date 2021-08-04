cd5e840c936b51925f448ab502bd8a15df60f278
배태호 <tdj03063@gmail.com>
2021-08-04 15:40:46 +0900

    Feat : Add create review, get reviews, delete review

---
+++ backend/src/main/java/com/ssafy/db/repository/ReviewRepository.java
@@ -0,0 +1,13 @@
+package com.ssafy.db.repository;
+
+import com.ssafy.db.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 방 모델 관련 디비 쿼리 생성을 위한 JPA Query Method 인터페이스 정의.
+ */
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+}
