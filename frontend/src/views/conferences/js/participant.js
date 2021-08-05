162e3d5a40fa8a3be2dfbcf99159a548bd582079
배태호 <tdj03063@gmail.com>
2021-08-05 18:36:47 +0900

    Fix : Kurento sendMessage Err

--- frontend/src/views/conferences/js/participant.js
+++ frontend/src/views/conferences/js/participant.js
@@ -1,5 +1,22 @@
-const PARTICIPANT_MAIN_CLASS = 'participant main';
-const PARTICIPANT_CLASS = 'participant';
+/*
+ * (C) Copyright 2014 Kurento (http://kurento.org/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+var PARTICIPANT_MAIN_CLASS = 'participant main';
+var PARTICIPANT_CLASS = 'participant';
 
 /**
  * Creates a video element for a new participant
@@ -9,77 +26,85 @@ const PARTICIPANT_CLASS = 'participant';
  *                        The tag of the new element will be 'video<name>'
  * @return
  */
-export default function Participant(pname) {
-  this.name = pname;
-  var container = document.createElement('div');
-  container.className = isPresentMainParticipant() ? PARTICIPANT_CLASS : PARTICIPANT_MAIN_CLASS;
-  container.id = pname;
-  var span = document.createElement('span');
-  var video = document.createElement('video');
-  var rtcPeer;
-  container.appendChild(video);
-  container.appendChild(span);
-  container.onclick = switchContainerClass;
-  document.getElementById('participants').appendChild(container);
+// eslint-disable-next-line no-unused-vars
+function Participant(name, sendMessage) {
+	this.name = name;
+	var container = document.createElement('div');
+	container.className = isPresentMainParticipant() ? PARTICIPANT_CLASS : PARTICIPANT_MAIN_CLASS;
+	container.id = name;
+	var span = document.createElement('span');
+	var video = document.createElement('video');
+	// eslint-disable-next-line no-unused-vars
+	var rtcPeer;
 
-  span.appendChild(document.createTextNode(this.name));
+	container.appendChild(video);
+	container.appendChild(span);
+	container.onclick = switchContainerClass;
+	document.getElementById('participants').appendChild(container);
 
-  video.id = 'video-' + this.name;
-  video.autoplay = true;
-  video.controls = false;
+	span.appendChild(document.createTextNode(name));
 
-
-  this.getElement = function() {
-    return container;
-  }
-
-  this.getVideoElement = function() {
-    return video;
-  }
-
-  function switchContainerClass() {
-    if (container.className === PARTICIPANT_CLASS) {
-      var elements = Array.prototype.slice.call(document.getElementsByClassName(PARTICIPANT_MAIN_CLASS));
-      elements.forEach(function(item) {
-          item.className = PARTICIPANT_CLASS;
-        });
-        container.className = PARTICIPANT_MAIN_CLASS;
-      } else {
-      container.className = PARTICIPANT_CLASS;
-    }
-  }
-
-  function isPresentMainParticipant() {
-    return ((document.getElementsByClassName(PARTICIPANT_MAIN_CLASS)).length != 0);
-  }
-
-  this.offerToReceiveVideo = function(error, offerSdp, wp){
-    if (error) return console.error ('sdp offer error')
-    console.log('Invoking SDP offer callback function');
-    var msg =  { id : 'receiveVideoFrom',
-        sender : this.name,
-        sdpOffer : offerSdp
-      };
-    sendMessage(msg);
-  }
+	video.id = 'video-' + name;
+	video.autoplay = true;
+	video.controls = false;
 
 
-  this.onIceCandidate = function (candidate, wp) {
-    console.log('Local candidate' + JSON.stringify(candidate));
+	this.getElement = function() {
+		return container;
+	}
 
-    var message = {
-      id: 'onIceCandidate',
-      candidate: candidate,
-      name: this.name
-    };
-    sendMessage(message);
-  }
+	this.getVideoElement = function() {
+		return video;
+	}
 
-  Object.defineProperty(this, 'rtcPeer', { writable: true});
+	function switchContainerClass() {
+		if (container.className === PARTICIPANT_CLASS) {
+			var elements = Array.prototype.slice.call(document.getElementsByClassName(PARTICIPANT_MAIN_CLASS));
+			elements.forEach(function(item) {
+					item.className = PARTICIPANT_CLASS;
+				});
 
-  this.dispose = function() {
-    console.log('Disposing participant ' + this.name);
-    this.rtcPeer.dispose();
-    container.parentNode.removeChild(container);
-  };
+				container.className = PARTICIPANT_MAIN_CLASS;
+			} else {
+			container.className = PARTICIPANT_CLASS;
+		}
+	}
+
+	function isPresentMainParticipant() {
+		return ((document.getElementsByClassName(PARTICIPANT_MAIN_CLASS)).length != 0);
+	}
+
+	this.offerToReceiveVideo = function(error, offerSdp/*, wp*/){
+		if (error) return console.error ("sdp offer error")
+		console.log('Invoking SDP offer callback function');
+		var msg =  { id : "receiveVideoFrom",
+				sender : name,
+				sdpOffer : offerSdp
+			};
+		sendMessage(msg);
+	}
+
+
+	this.onIceCandidate = function (candidate/*, wp*/) {
+		console.log("Local candidate" + JSON.stringify(candidate));
+
+		var message = {
+			id: 'onIceCandidate',
+			candidate: candidate,
+			name: name
+		};
+		sendMessage(message);
+	}
+
+	Object.defineProperty(this, 'rtcPeer', { writable: true});
+
+	this.dispose = function() {
+		console.log('Disposing participant ' + this.name);
+		this.rtcPeer.dispose();
+		container.parentNode.removeChild(container);
+	};
+}
+
+export {
+  Participant
 }
