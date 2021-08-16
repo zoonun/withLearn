76392942dd63328b04f1d14a26e670a8d161a142
lalua456 <lalua456@gmail.com>
2021-08-16 23:39:58 +0900

    Style: Community

---
+++ frontend/src/views/home/components/community.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="community-header">
+    커뮤니티
+  </div>
+</template>
+<style>
+.detail-header {
+  background-color:#5a4ae3;
+  height: 4rem;
+  color:white;
+  padding-left: 10rem;
+  padding-right: 10rem;
+  padding-top: 1.5rem;
+  font-size: 2.25rem;
+  line-height: 2.5rem;
+  font-weight: bold;
+}
+.detail-thumbnail {
+  /* object-fit:cover; */
+  width:200px;
+}
+.detail-information {
+  display: flex;
+  justify-content: space-between;
+  margin-right:30rem;
+  margin-left:30rem;
+  font-size: 2.25rem;
+  margin-top:8rem;
+}
+.detail-isactive {
+  display: flex;
+  flex-direction: column;
+  font-weight: bold;
+}
+.live-button {
+  margin-top:1rem;
+  font-size: 2.25rem;
+  color: white;
+  background-color: #5a4ae3;
+  padding: 5px;
+  border-radius: 5px;
+  border:0;
+  cursor: pointer;
+}
+.detail-bottom {
+  margin-top:2rem;
+  border-width:1rem;
+  margin-right:20rem;
+  margin-left:20rem;
+  font-size: 2.25rem;
+}
+.detail-thumbnail-wrapper {
+  /* background-size: cover; */
+  display: flex;
+  justify-content: center;
+}
+.detail-bottom-button {
+  margin-top:1rem;
+  font-size: 2.25rem;
+  color: black;
+  background-color: #e5e7eb;
+  padding: 5px;
+  border-radius: 5px;
+  border:0;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+}
+.detail-list {
+  list-style:none;
+  border: 2px solid #e5e7eb;
+  padding:2rem;
+
+}
+.detail-description {
+  width:60%;
+  font-weight:bold;
+}
+
+</style>
+
+<script>
+import { reactive, onMounted, onUnmounted, computed } from 'vue'
+import { useStore } from 'vuex'
+import { useRoute } from 'vue-router'
+
+export default {
+  name: 'community',
+
+  setup () {
+    const route = useRoute()
+    const store = useStore()
+    const state = reactive({
+      conferenceId: '',
+    })
+
+    // 페이지 진입시 불리는 훅
+    onMounted(() => {
+      state.conferenceId = route.params.conferenceId
+      store.dispatch('root/requestConferenceDetail', state.conferenceId)
+    })
+
+    // 페이지 이탈시 불리는 훅
+    onUnmounted(() => {
+      state.conferenceId = ''
+    })
+
+    return { state }
+  }
+}
+</script>
+
